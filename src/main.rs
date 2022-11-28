68ae6d6a0c2fa86f2dbddfa512ccb420cbfa26f4
AlejandroRNatal <rodrigueznatalandy@gmail.com>
2022-11-28 02:24:23 -0500

    [INT] Added basic interrupt handling

--- src/main.rs
+++ src/main.rs
@@ -3,56 +3,41 @@
 #![feature(core_intrinsics)]
 #![feature(custom_test_frameworks)]
 #![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
 use core::intrinsics;
 use core::panic::PanicInfo;
 
-mod vga_buffer;
+use titan::{print, println};
 
-#![reexport_test_harness_main = "test_main"]
 
-#[cfg(test)]
-fn test_runner(tests: &[&dyn Fn()]) {
-    println!("Running {} tests...", tests.len());
-    for test in tests {
-        test();
-    }
-}
-
+#[cfg(not(test))]
 #[panic_handler]
 #[no_mangle]
 fn panic(_info: &PanicInfo) -> ! {
-    println!("{}", info);
+    print!("{}", _info);
     loop {}
 }
 
-fn buffer() {
-
-    static HELLO: &[u8] = b"Hello World!";
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in HELLO.iter().enumerate() {
-      unsafe {
-        *vga_buffer.offset(i as isize * 2) = byte;
-          *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-      }
-    }
-    
+#[cfg(test)]
+#[panic_handler]
+#[no_mangle]
+fn panic(_info: &PanicInfo) -> ! {
+    titan::test_panic_handler(_info)
 }
+
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     
-    //let msg = b"Hello, World!";
-    
-    //let mut cursor = Cursor {
-      //  position: 0,
-        //foreground: Color::BrightCyan,
-        //background: Color::Black,
-    //};
-    
-    //cursor.print(msg);
     println!("Hello from a raw Print Macro!");
+    
+    titan::init(); 
+
+    //x86_64::instructions::interrupts::int3(); 
+    unsafe {
+        *(0xdeadbeef as *mut u64) = 69; //lil hacker ref deadbeef;
+    };
 
     #[cfg(test)]
     test_main();
@@ -60,4 +45,11 @@ pub extern "C" fn _start() -> ! {
 
     loop {}
 } 
+
+
+#[test_case]
+fn trivial_assertion() {
+    assert_eq!(1, 1);
+}
+
 
